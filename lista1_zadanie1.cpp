deff99111662e3cf7339ec205c0cb97c512e6414
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-02-23 23:48:25 +0100

    Create lista1_zadanie1.cpp

---
+++ lista1_zadanie1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+    std::string name, surname;
+    std::cout << "Podaj imię \n";
+    std::cin >> name;
+    std::cout << "Podaj nazwisko \n";
+    std::cin >> surname;
+    std::cout << name << " " << surname << "\n";
+    std::getchar();
+    return 0;
+}
