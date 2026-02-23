b68dd4e7726a34d12e72bd27d0c690245939b39c
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-02-24 00:37:17 +0100

    Create lista2_zadanie4.cpp

---
+++ lista2_zadanie4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+    char first_char = 0xC4;
+    char second_char = 0x84;
+    char third_char = 0x0a;
+    std::cout << first_char << "\n";
+    std::cout << second_char << "\n";;
+    std::cout << third_char << "\n";;
+    return 0;
+}
