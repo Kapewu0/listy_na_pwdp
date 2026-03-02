d7747698c044412496c16361632bc96ea11a4463
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-03 00:49:12 +0100

    final version

--- lista2_zadanie3.cpp
+++ lista2_zadanie3.cpp
@@ -1,7 +1,17 @@
+/*
+    Konrad Marciniak
+    Lista 2 Zadanie 3
+    Pod tym linkiem w serwisie Stack Overflow: https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal 
+    znaleźć można odpowiedzi na pytanie, w jaki sposób można wyświetlić na ekranie terminala kolorowy tekst.
+    Używając zawartych tam informacji napisz program, który wypisze twoje imię czcionką zieloną,
+    a nazwisko - czerwoną. Uwaga: różne terminale różnie implementują sposoby zarządzania kolorem w terminalu.
+    Użytkownicy systemu Windows używają go na własne ryzyko.
+
+*/
+
 #include <iostream>
 int main() {
     std::string green_color = "\033[1;32m";
     std::string red_color = "\033[1;31m";
     std::cout << green_color << "Konrad " << red_color << "Marciniak";
-    return 0;
 }
