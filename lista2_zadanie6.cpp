f2832f0e248b00a1b203231f1a1581878c20abcb
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-03 00:57:25 +0100

    final version

--- lista2_zadanie6.cpp
+++ lista2_zadanie6.cpp
@@ -1,19 +1,21 @@
+/*
+    Konrad Marciniak
+    Lista 2 Zadanie 6
+    Napisz program, który z konsoli wczyta napis do std::string i wyświetli jego postać w kodowaniu Base16.
+    Brzmi strasznie, ale tak naprawdę chodzi o to, by każdy znak wyświetlić w reprezentacji szesnastkowej.
+    Najprostszy sposób przejrzenia zawartości obiektu napis typu std::string litera po literze:
+
+*/
+
 #include <iostream>
 int main() {
-    std::cout << "Podaj napis, ktory chcesz zapisac w postaci szesnastkowej \n";
+    std::cout << "Podaj napis, który chcesz zapisać w postaci szesnastkowej: \n";
     std::string given_string;
     std::cin >> given_string;
-    while (given_string.length() == 0)
-    {
-        std::cout << "Niepoprawna ilosc znakow" << std::endl;
-        std::cin >> given_string;
-    }
 
-    
     for (int i = 0; i < given_string.length(); i++)
     {
         std::printf("%x", given_string[i]);
     }
     
-    return 0;
 }
