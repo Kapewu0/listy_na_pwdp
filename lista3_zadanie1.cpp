ec65eae2186c0a7581dc0f446e00cb17655ed3de
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-02 11:23:49 +0100

    Create lista3_zadanie1.cpp

---
+++ lista3_zadanie1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(){
+std::cout << "Podaj słowo, które chcesz wypisać wspak" << std::endl;
+std::string prompted_string;
+std::fin << prompted_string;
+for (int i= prompted_string.length() -1; i>= 0; i--;
+{
+std::cout << prompted_string[i];
+}
+std::cout<< std::endl;
+}
