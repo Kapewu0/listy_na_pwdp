9b1e6cffc7e8c31666eeef766e836d4de1a393bb
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-02 11:10:09 +0100

    Update lista2_zadanie4.cpp

--- lista2_zadanie4.cpp
+++ lista2_zadanie4.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 int main() {
-    char first_char = 0xC4;
-    char second_char = 0x84;
-    char third_char = 0x0a;
-    std::cout << first_char << "\n";
-    std::cout << second_char << "\n";;
-    std::cout << third_char << "\n";;
+    printf("%c", char(0xC4));
+    printf("%c", char(0x84));
+    printf("%c", char(0x0a));
     return 0;
 }
