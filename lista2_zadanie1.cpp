91743e081630242633e9a554d112a251e088f738
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-03 00:23:20 +0100

    final version

--- lista2_zadanie1.cpp
+++ lista2_zadanie1.cpp
@@ -1,3 +1,16 @@
+/*
+    Konrad Marciniak
+    Lista 2 Zadanie 1 
+    Napisz program, który:
+
+    Wczytuje imię
+    ­­■ Prosi użytkownika o podanie imienia
+    ■ Prosi użytkownika o podanie nazwiska
+    ■ Wczytuje nazwisko
+    ■ Wyświetla imię i nazwisko
+
+*/
+
 #include <iostream>
 int main() {
     std::string name, surname;
@@ -6,5 +19,4 @@ int main() {
     std::cout << "Podaj nazwisko \n";
     std::cin >> surname;
     std::cout << name << " " << surname << "\n";
-    return 0;
 }
