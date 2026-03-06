4d42424cdc0fca35954f5d3a4cc70789fa3a555d
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-06 22:06:15 +0100

    final version

--- lista2_zadanie3.cpp
+++ lista2_zadanie3.cpp
@@ -13,5 +13,5 @@
 int main() {
     std::string green_color = "\033[1;32m";
     std::string red_color = "\033[1;31m";
-    std::cout << green_color << "Konrad " << red_color << "Marciniak";
+    std::cout << green_color << "Konrad " << red_color << "Marciniak" << "\033[0m";
 }
