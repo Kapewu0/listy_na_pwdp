62e858cea27c9666c56c51502db7a3ac2358dc4d
Kapewu <70098513+Kapewu0@users.noreply.github.com>
2026-03-06 22:19:20 +0100

    final version

--- lista2_zadanie5.cpp
+++ lista2_zadanie5.cpp
@@ -1,5 +1,16 @@
+/*
+    Konrad Marciniak
+    Lista 2 Zadanie 5
+    Użyj std::format lub std::print, by wyświetlić szesnastkową wartość znaku przejścia do nowej linii: '\n'.
+    Jaki jest związek uzyskanej odpowiedzi z treścią poprzedniego zadania?
+    
+    Odpowiedź na pytanie
+    Wykorzystany znak o wartości szesnastkowej 0a był znakiem przejścia do nowej linii.
+*/
+
 #include <iostream>
+#include <format>
+
 int main() {
-    printf("%#x", '\n');
-    return 0;
+    std::cout << std::format("{:#04x}", '\n');
 }
